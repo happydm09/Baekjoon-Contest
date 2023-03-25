42a01cf4cd65cf99006790a8aa29e620714791a0
DM <100419204+happydm09@users.noreply.github.com>
2023-03-25 20:47:02 +0900

    Create 978-1(A).py

---
+++ 978-1(A).py
@@ -0,0 +1,5 @@
+names = {'NLCS': 'North London Collegiate School',
+         'KIS': 'Korea International School',
+         'BHA': 'Branksome Hall Asia',
+         'SJA': 'St. Johnsbury Academy'}
+print(names[input()])
