a2b97a969c5e0a440f23fbdb85d235f92a2472e0
DM <100419204+happydm09@users.noreply.github.com>
2023-04-08 14:46:38 +0900

    Update 963-1(A).py

--- 963-1(A).py
+++ 963-1(A).py
@@ -5,9 +5,8 @@ m, k = map(int, input().split())
 for i in range(m):
     h = list(map(int, input().split()))
     for e in range(len(h)):
-        n = egg_plant[h[e] - 1]
-        h[e] = n
-    if str(h).find('P') == -1:
+        if egg_plant[h[e] - 1] == 'P': break
+    else:
         print('W')
         break
 else:
