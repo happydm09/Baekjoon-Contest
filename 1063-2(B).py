c73522fed716da65a4aa61aec20c45d718cafa6c
DM <happydm09@naver.com>
2023-07-06 19:28:32 +0900

    Create 1063-2(B).py

---
+++ 1063-2(B).py
@@ -0,0 +1,16 @@
+w = input()
+
+if w in ["fdsajkl;", "jkl;fdsa"]:
+    print('in-out')
+
+elif w in ['asdf;lkj', ';lkjasdf']:
+    print('out-in')
+
+elif w == 'asdfjkl;':
+    print('stairs')
+
+elif w == ';lkjfdsa':
+    print('reverse')
+
+else:
+    print('molu')
